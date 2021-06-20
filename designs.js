8848ec24271c01cd80df3add70fed333134bdf77
emma wolfe <emwolfe@emmas-mbp.home>
2021-06-20 11:39:49 +0100

    Add comment for cell event listeners

--- designs.js
+++ designs.js
@@ -1,11 +1,12 @@
 
-// Set variables for the canvas and colour picker
+// Set variables for the canvas, colour picker & form
 const myTable = document.getElementById('pixelCanvas');
 const fillColour = document.getElementById('colorPicker');
 const form = document.getElementById('sizePicker');
 
 //Define function to create the grid
 function makeGrid(rows,cells) {
+
     //Re-set table back to original state
     myTable.innerHTML = '';
 
@@ -14,14 +15,20 @@ function makeGrid(rows,cells) {
 
     //Create the grid
     for(let r = 0; r < rows; r++) {
+
         //Create a row
         const row = document.createElement('tr');
+
         //Create cells & append to row
         for (let c = 0; c < cells; c++) {
             const cell = document.createElement('td');
+
+            //Add listeners to cells to change background colour
             cell.addEventListener('click', function (evt) {
                 evt.target.style.backgroundColor = fillColour.value;
             })
+
+            //Append the cells to the row
             row.appendChild(cell);
         }
         //Append the row to the table body
@@ -31,7 +38,7 @@ function makeGrid(rows,cells) {
     myTable.appendChild(tableBody);
 };
 
-// Listen for sizePicker form submission & then trigger makeGrid()
+// Listen for form submission & then trigger makeGrid()
 form.addEventListener('submit', function (evt) {
     const rows = document.getElementById('inputHeight').value;
     const cells = document.getElementById('inputWidth').value;
